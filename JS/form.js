3c29c1ad113001f165bf82690b85a08f4f8134bb
snehalupatil <patilsnehal4575@gmail.com>
2021-06-25 23:03:34 +0530

    Added: CRUD operation of Employee with validations

--- JS/form.js
+++ JS/form.js
@@ -1,21 +1,145 @@
 
-let valid = false;
+var selectedRow = null
+
 function onFormSubmit() {
+    alert("save");
     if (validate()) {
-        valid = true;
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["firstName"] = document.getElementById("firstName").value;
+    formData["lastName"] = document.getElementById("lastName").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.firstName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.lastName;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.email;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.city;
+    cell5 = newRow.insertCell(5);
+    cell5.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+ 
+}
+
+function resetForm() {
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("city").value = "";
+    selectedRow = null;
+    
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("firstName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("lastName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[4].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.firstName;
+    selectedRow.cells[1].innerHTML = formData.lastName;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.email;
+    selectedRow.cells[4].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
     }
 }
 
 
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.getElementById("firstName").value == "") {
         isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
+        document.getElementById("firstNameError").innerText = "Please Enter First Name"
     } else {
         isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
+        document.getElementById("firstNameError").innerText = ""
+        
+    }
+
+    if (document.getElementById("lastName").value == "") {
+        isValid = false;
+        document.getElementById("lastNameError").innerText = "Please Enter Last Name"
+    } else {
+        isValid = true;
+        document.getElementById("lastNameError").innerText = ""
+        
+    }
+
+    if (document.getElementById("salary").value == "") {
+        document.getElementById("salaryError").innerText = "Please Enter Salary"
+    } else {
+        isValid = true;
+        document.getElementById("salaryError").innerText = ""
+        
+    }
+
+    if (document.getElementById("email").value == "") {
+        isValid = false;
+        document.getElementById("emailError").innerText = "Please Enter Email"
+    } else {
+        isValid = true;
+        document.getElementById("emailError").innerText = ""
+        
+    }
+
+    if (document.getElementById("city").value == "") {
+        isValid = false;
+        document.getElementById("cityError").innerText = "Please Enter City"
+    } else {
+        isValid = true;
+        document.getElementById("cityError").innerText = ""
+        
     }
     return isValid;
+}
+
+addEmployee = (data) => {
+    ajaxCalling('post', 'http://localhost:3000/employees', data)
+}
+
+ajaxCalling = (type, url, data) => {
+    $.ajax({
+        type: type,
+        url: url,
+        data: data,
+        success: function (result) {
+            console.log(result);
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    })
 }
